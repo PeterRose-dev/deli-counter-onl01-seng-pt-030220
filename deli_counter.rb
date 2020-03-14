0c6119a61bf3879c95f85e8c50359f6904af725a
Peter Rose <peterroseq1@gmail.com>
2020-03-14 12:05:08 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -16,17 +16,40 @@ array = ["Amanda", "Annette", "Ruchi", "Jason", "Logan", "Spencer", "Avi", "Joe"
 line(array)
 
 def take_a_number(line, customer)
+<<<<<<< HEAD
   line.push(customer)
    puts "Welcome, #{customer}. You are number #{line.length} in line."
   end
   
      
+=======
+  if line.length == 0
+      line.push("#{customer}")
+      puts "Welcome, #{customer}. You are number 1 in line."
+  else
+    line.length > 0
+    counter = line.length
+    while
+      counter > 0
+      line_messages = []
+      line.push("#{customer}")
+      line.reverse.each do |customer|
+        puts "Welcome, #{customer}. You are number #{counter+1} in line."
+        line_messages << line.pop
+        counter -=1
+      end
+      puts line_messages.join(" ")
+    end
+  end
+end
+>>>>>>> 220f4a2e249bb21d1ff966ffc8cc77ed622e16f2
 
 other_deli = ["Logan", "Avi", "Spencer"]
 take_a_number(other_deli,"Grace")
 
 def now_serving(array)
   if array.length == 0
+<<<<<<< HEAD
      puts "There is nobody waiting to be served!"
   else
      puts "Currently serving #{array.first}."
@@ -34,6 +57,21 @@ def now_serving(array)
     end
   end
 
+=======
+      puts "There is nobody waiting to be served!"
+  else
+    counter = 0
+    array2 =[]
+    while counter < array.length
+    array2 << array.shift
+      array2.each do |customer| puts "Currently serving #{customer}."
+      counter +=1
+      end
+      puts array.join(" ")
+    end
+  end
+end
+>>>>>>> 220f4a2e249bb21d1ff966ffc8cc77ed622e16f2
 
 other_deli = ["Logan", "Avi", "Spencer"]
 now_serving(other_deli)
